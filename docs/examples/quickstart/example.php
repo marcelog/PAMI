bc1262caba5761e01d614b5c03cc72108b9953d8
Marcelo Gornstein <marcelog@gmail.com>
2011-01-04 10:48:03 -0300

    added CoreSettingsAction

--- docs/examples/quickstart/example.php
+++ docs/examples/quickstart/example.php
@@ -43,6 +43,7 @@ use PAMI\Message\Action\ReloadAction;
 use PAMI\Message\Action\CommandAction;
 use PAMI\Message\Action\HangupAction;
 use PAMI\Message\Action\SIPShowRegistryAction;
+use PAMI\Message\Action\CoreSettingsAction;
 
 class A implements IEventListener
 {
@@ -68,6 +69,7 @@ try
 	var_dump($a->send(new StatusAction()));
 	var_dump($a->send(new CommandAction('sip show peers')));
 	var_dump($a->send(new SIPShowRegistryAction()));
+	var_dump($a->send(new CoreSettingsAction()));
 	//var_dump($a->send(new HangupAction('SIP/XXXX-123123')));
 	//var_dump($a->send(new ReloadAction()));
 	//var_dump($a->send(new ReloadAction('chan_sip')));
