e5df9c88e29f6bc51bdee1630b767cf18914d7db
Marcelo Gornstein <marcelog@gmail.com>
2011-01-04 13:27:19 -0300

    commented out examples (just in case) of dahdidndon and dahdidndoff

--- docs/examples/quickstart/example.php
+++ docs/examples/quickstart/example.php
@@ -92,8 +92,8 @@ try
 	var_dump($a->send(new GetConfigAction('sip.conf', 'general')));
 	var_dump($a->send(new GetConfigJSONAction('extensions.conf')));
 	var_dump($a->send(new DAHDIShowChannelsAction()));
-	var_dump($a->send(new DAHDIDNDOnAction('1')));
-	var_dump($a->send(new DAHDIDNDOffAction('1')));
+	//var_dump($a->send(new DAHDIDNDOnAction('1')));
+	//var_dump($a->send(new DAHDIDNDOffAction('1')));
 	//var_dump($a->send(new DAHDIDialOffHookAction(1, '113')));
 	//var_dump($a->send(new DAHDIRestartAction()));
 	//var_dump($a->send(new RedirectAction('SIP/a-1', '51992266', 'netlabs', '1')));
