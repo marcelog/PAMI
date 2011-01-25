ed46f1a3f6ebc1d126c52f722fc04416b0e5a9cb
Marcelo Gornstein <marcelog@gmail.com>
2011-01-25 16:51:09 -0300

    added Events Action (currently broken because asterisk violates -yet again- the ami protocol, not sending the EOM, which is \r\n\r\n)

--- src/mg/PAMI/Message/Action/EventsAction.php
+++ src/mg/PAMI/Message/Action/EventsAction.php
@@ -1,1 +1,47 @@
 <?php
+/**
+ * Events action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://www.noneyet.ar/
+ */
+namespace PAMI\Message\Action;
+
+/**
+ * Events action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
+class EventsAction extends ActionMessage
+{
+    /**
+     * Constructor.
+     *
+     * @param string[] $mask Asterisk events to handle (system, call, log, etc).
+     *
+     * @return void
+     */
+    public function __construct(array $mask = array())
+    {
+        parent::__construct('Events');
+        if (empty($mask)) {
+            $this->setKey('EventMask', 'off');
+        } else {
+            $this->setKey('EventMask', implode(',', $mask));
+        }
+    }
+}
