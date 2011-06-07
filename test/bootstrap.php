2d94ce38e0667ed404cb9e1ffbc5de27faf00ab8
Marcelo Gornstein <marcelog@gmail.com>
2011-06-07 14:11:37 -0300

    started work on unit tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,9 +5,17 @@ ini_set(
         PATH_SEPARATOR,
         array(
             ini_get('include_path'),
-            implode(DIRECTORY_SEPARATOR, array('..', 'src', 'mg'))
+            realpath(implode(
+                DIRECTORY_SEPARATOR, array(__DIR__, '..', 'src', 'mg')
+            ))
         )
     )
 );
-require_once 'PAMI/Autoloader/Autoloader.php'; // Include ding autoloader.
-\PAMI\Autoloader\Autoloader::register(); // Call autoloader register for ding autoloader.
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once 'PAMI/Logger/Logger.php';
+if (!defined('RESOURCES_DIR')) {
+    define ('RESOURCES_DIR', realpath(__DIR__) . DIRECTORY_SEPARATOR . 'resources');
+}
+require_once 'PAMI/Autoloader/Autoloader.php';
+\PAMI\Autoloader\Autoloader::register();
