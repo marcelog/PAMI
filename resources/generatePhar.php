f0da859ddee00a0888a7eeeb9acd638251be1d6a
Marcelo Gornstein <marcelog@gmail.com>
2012-02-09 11:52:24 -0300

    fixed phar autoloader

--- resources/generatePhar.php
+++ resources/generatePhar.php
@@ -2,6 +2,13 @@
 $stub =
 '<?php
 Phar::mapPhar();
+spl_autoload_register(function ($class) {
+    $classFile = "phar://pami.phar/" . str_replace("\\\", "/", $class) . ".php";
+    if (file_exists($classFile)) {
+        require_once $classFile;
+        return true;
+    }
+});
 include "phar://pami.phar/PAMI/Autoloader/Autoloader.php";
 \PAMI\Autoloader\Autoloader::register();
 __HALT_COMPILER();
@@ -10,3 +17,4 @@ $phar = new Phar($argv[1]);
 $phar->setAlias('pami.phar');
 $phar->buildFromDirectory($argv[2]);
 $phar->setStub($stub);
+
