e4fe420b6c4d2784f1623fb978447febc28539d2
Jacob Kiers <jacob@jacobkiers.net>
2012-06-29 12:57:05 +0200

    Added test for CELEvent

--- test/events/Test_Events.php
+++ test/events/Test_Events.php
@@ -73,7 +73,7 @@ class Test_Events extends \PHPUnit_Framework_TestCase
             'Agents', 'AgentsComplete', 'Agentlogoff', 'Agentlogin', 'AgentConnect',
             'DongleSMSStatus', 'FullyBooted', 'DongleShowDevicesComplete', 'DongleDeviceEntry',
             'DongleNewUSSDBase64', 'DongleNewUSSD', 'DongleUSSDStatus', 'DongleNewCUSD',
-            'DongleStatus'
+            'DongleStatus', 'CEL'
         );
         $eventTranslatedValues = array(
             'QueueMemberStatus' => array(
@@ -698,7 +698,32 @@ class Test_Events extends \PHPUnit_Framework_TestCase
         	    'X-vGSM-ME-State-Change-Reason' => 'X-vGSM-ME-State-Change-Reason',
         	    'X-vGSM-ME-Old-State' => 'X-vGSM-ME-Old-State',
         	    'X-vGSM-ME-State' => 'X-vGSM-ME-State',
-        	),
+                ),
+                'CEL' => array(
+                    'AMAFlags' => 'AMAFlags',
+                    'AccountCode' => 'AccountCode',
+                    'AppData' => 'AppData',
+                    'Application' => 'Application',
+                    'CallerIDani' => 'CallerIDani',
+                    'CallerIDdnid' => 'CallerIDdnid',
+                    'CallerIDname' => 'CallerIDname',
+                    'CallerIDnum' => 'CallerIDnum',
+                    'CallerIDrdnis' => 'CallerIDrdnis',
+                    'Channel' => 'Channel',
+                    'Context' => 'Context',
+                    'Event' => 'Event',
+                    'EventName' => 'EventName',
+                    'EventTime' => 'EventTime',
+                    'Exten' => 'Exten',
+                    'Extra' => 'Extra',
+                    'LinkedID' => 'LinkedID',
+                    'Peer' => 'Peer',
+                    'PeerAccount' => 'PeerAccount',
+                    'Privilege' => 'Privilege',
+                    'Timestamp' => 'Timestamp',
+                    'UniqueID' => 'UniqueID',
+                    'Userfield' => 'Userfield',
+                ),
         );
         $eventGetters = array(
             'AsyncAGI' => array(
@@ -762,7 +787,7 @@ class Test_Events extends \PHPUnit_Framework_TestCase
         		'X-vGSM-ME-State-Change-Reason' => 'Reason',
         	    'X-vGSM-ME-Old-State' => 'OldState',
         	    'X-vGSM-ME-State' => 'State',
-        	)
+        	),
         );
         foreach ($eventNames as $eventName) {
             $this->_testEvent($eventName, $eventGetters, $eventValues[$eventName], $eventTranslatedValues);
