5d998e1a05ea8d385350ac08855fa7b60bb6fdd0
Jacob Kiers <jacob@alphacomm.nl>
2012-07-12 13:38:58 +0200

    Add possibility to set Action ID.

--- src/mg/PAMI/Message/Action/ActionMessage.php
+++ src/mg/PAMI/Message/Action/ActionMessage.php
@@ -29,6 +29,7 @@
 namespace PAMI\Message\Action;
 
 use PAMI\Message\OutgoingMessage;
+use PAMI\Exception\PAMIException;
 
 /**
  * A generic action ami message.
@@ -55,5 +56,26 @@ abstract class ActionMessage extends OutgoingMessage
         parent::__construct();
         $this->setKey('Action', $what);
         $this->setKey('ActionID', microtime(true));
+    }
+
+    /**
+     * Sets Action ID.
+     *
+     * The ActionID can be at most 69 characters long, according to
+     * {@link https://issues.asterisk.org/jira/browse/14847 Asterisk Issue 14847}.
+     * 
+     * Therefore we'll throw an exception when the ActionID is too long.
+     *
+     * @param $actionID The Action ID to have this action known by
+     *
+     * @return void
+     * @throws PAMIException When the ActionID is more then 69 characters long
+     */
+    public function setActionID($actionID)
+    {
+        if (strlen($actionID) > 69) {
+            throw new PAMIException('ActionID can be at most 69 characters long.');
+        }
+        $this->setKey('ActionID', $actionID);
     }
 }
