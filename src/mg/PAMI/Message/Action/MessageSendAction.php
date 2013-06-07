bde740e73556e967d034ec8e230fa2564d347833
pkirk <paolo.gabrielli@gmail.com>
2013-06-07 15:56:11 +0200

    MessageSend, for Asterisk >= 11
    Tried and seems to work: message are sent and received.
    Here the "specify":
    https://wiki.asterisk.org/wiki/display/AST/Asterisk+11+ManagerAction_MessageSend
    I think it need some logic.

---
+++ src/mg/PAMI/Message/Action/MessageSendAction.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * MessageSend action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://marcelog.github.com/PAMI/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+namespace PAMI\Message\Action;
+
+/**
+ * MessageSend action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @link       http://marcelog.github.com/PAMI/
+ */
+class MessageSendAction extends ActionMessage
+{
+    /**
+     * Constructor.
+     *
+     * @param string $channel Channel to send message to.
+     * @param string $message Message to send.
+     *
+     * @return void
+     */
+    public function __construct($to, $from, $body, $base64body)
+    {
+        parent::__construct('MessageSend');
+        $this->setKey('To', $to);
+        $this->setKey('From', $from);
+        $this->setKey('Body', $body);
+        $this->setKey('Base64Body, $base64body);
+    }
+}
