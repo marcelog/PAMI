fc205e33c92eb733ce99fb7634f5b62fa8f2644b
Diederik de Groot <ddegroot@talon.nl>
2015-04-06 20:51:32 +0200

    Fix comments and indentation of ResponseFactoryImpl

--- src/mg/PAMI/Message/Response/Factory/Impl/ResponseFactoryImpl.php
+++ src/mg/PAMI/Message/Response/Factory/Impl/ResponseFactoryImpl.php
@@ -8,12 +8,12 @@
  * @category   Pami
  * @package    Response
  * @subpackage Factory.Impl
- * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @author     Diederik de Groot <ddegroot@users.sf.net>
  * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
  * @version    SVN: $Id$
  * @link       http://marcelog.github.com/PAMI/
  *
- * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ * Copyright 2015 Diederik de Groot <ddegroot@users.sf.net>, Marcelo Gornstein <marcelog@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,32 +35,33 @@ use PAMI\Message\Response\GenericResponse;
 use PAMI\Message\Message;
 
 /**
- * This factory knows which event to return according to a given raw message
- * from ami.
+ * This factory knows which reponse handler to return according to a given raw message from ami,
+ * the outgoingMessageClass and responseHandler requested by the Action
  *
  * PHP Version 5
  *
  * @category   Pami
  * @package    Response
  * @subpackage Factory.Impl
- * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @author     Diederik de Groot <ddegroot@users.sf.net>
  * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
  * @link       http://marcelog.github.com/PAMI/
  */
 class ResponseFactoryImpl
 {
-    /**
-     * This is our factory method.
-     *
-     * @param string $message Literal message as received from ami.
-     *
-     * @return EventMessage
-     */
-    public static function createFromRaw($logger, $message, $outgoingMessageClass = false, $responseHandler = false)
-    {
-        
-        if ($outgoingMessageClass != false && $responseHandler == false) {
-            $responseHandler = substr($outgoingMessageClass, 20, -6);
+	/**
+	 * This is our factory method.
+	 *
+	 * @param string $message Literal message as received from ami.
+	 * @param string $outgoingMessageClass ClassName
+	 * @param string $responseHandler
+	 *
+	 * @return EventMessage
+	 */
+	public static function createFromRaw($logger, $message, $outgoingMessageClass = false, $responseHandler = false)
+	{
+		if ($outgoingMessageClass != false && $responseHandler == false) {
+			$responseHandler = substr($outgoingMessageClass, 20, -6);
 		}
 		if ($responseHandler != false) {
 			$className = '\\PAMI\\Message\\Response\\' . $responseHandler . 'Response';
@@ -70,16 +71,16 @@ class ResponseFactoryImpl
 				}
 				return new $className($message);
 			}
-        }
-	    return new GenericResponse($message);
-    }
+		}
+		return new GenericResponse($message);
+	}
 
-    /**
-     * Constructor. Nothing to see here, move along.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
+	/**
+	 * Constructor. Nothing to see here, move along.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+	}
 }
