3d746be5c59cb33abae4727d0c2605f7b5831cd3
Diederik de Groot <ddegroot@talon.nl>
2015-04-07 22:22:38 +0200

    Added Actual SCCP Events

---
+++ src/mg/PAMI/Message/Event/CallForwardEvent.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Event triggered when the 'SCCP CallForward' Event arrives.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Event
+ * @author     Diederik de Groot <ddegroot@users.sf.net>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://marcelog.github.com/PAMI/
+ *
+ * Copyright 2015 Diederik de Groot <ddegroot@users.sf.net>, Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+namespace PAMI\Message\Event;
+
+use PAMI\Message\Event\EventMessage;
+
+/**
+ * Event triggered when the 'SCCP CallForward' Event arrives.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Event
+ * @author     Diederik de Groot <ddegroot@users.sf.net>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @link       http://marcelog.github.com/PAMI/
+ */
+class CallForwardEvent extends EventMessage
+{
+	/**
+	 * Returns key: 'ChannelType'.
+	 *
+	 * @return string
+	 */
+	public function getChannelType()
+	{
+      return $this->getKey('ChannelType');
+	}
+
+	/**
+	 * Returns key: 'ChannelObjectType'.
+	 *
+	 * @return string
+	 */
+	public function getChannelObjectType()
+	{
+      return $this->getKey('ChannelObjectType');
+	}
+
+	/**
+	 * Returns key: 'Feature'.
+	 *
+	 * @return string
+	 */
+	public function getFeature()
+	{
+      return $this->getKey('Feature');
+	}
+
+	/**
+	 * Returns key: 'Status'.
+	 *
+	 */
+	public function getStatus()
+	{
+	}
+
+	/**
+	 * Returns key: 'SCCPLine'.
+	 *
+	 * @return string
+	 */
+	public function getSCCPLine()
+	{
+      return $this->getKey('SCCPLine');
+	}
+
+	/**
+	 * Returns key: 'SCCPDevice'.
+	 *
+	 * @return string
+	 */
+	public function getSCCPDevice()
+	{
+      return $this->getKey('SCCPDevice');
+	}
+
+}
