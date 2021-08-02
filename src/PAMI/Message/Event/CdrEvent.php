f7a4fa210db5d14bb303d159a272b9129d593f81
Nattapong <nattapong@octagon.co.th>
2021-08-02 19:31:54 +0700

    Fixed class name CdrEvent

--- src/PAMI/Message/Event/CdrEvent.php
+++ src/PAMI/Message/Event/CdrEvent.php
@@ -43,7 +43,7 @@ use PAMI\Message\Event\EventMessage;
  * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
  * @link       http://marcelog.github.com/PAMI/
  */
-class ChannelUpdateEvent extends EventMessage
+class CdrEvent extends EventMessage
 {
     /**
      * Returns key: 'Privilege'.
