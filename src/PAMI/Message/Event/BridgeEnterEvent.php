2502c22e9dce8c70cd1208de57577a975a21bcd5
Morvai Szabolcs <smorvai@arenim.com>
2022-03-25 15:16:43 +0100

    add LinkedId to bridge enter event

--- src/PAMI/Message/Event/BridgeEnterEvent.php
+++ src/PAMI/Message/Event/BridgeEnterEvent.php
@@ -64,6 +64,16 @@ class BridgeEnterEvent extends EventMessage
     }
 
     /**
+     * Returns key: 'Linkedid'.
+     *
+     * @return string
+     */
+    public function getLinkedid()
+    {
+        return $this->getKey('Linkedid');
+    }
+
+    /**
      * Returns key: 'BridgeType'.
      *
      * @return string
