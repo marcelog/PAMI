8a6483d1aac71d3682489a72b4c643c800aa6b01
MoMarCode IT <office@momarcodeit.co.rs>
2022-04-12 09:28:16 +0200

    add PJSIPShowEndpoint

---
+++ src/PAMI/Message/Action/PJSIPShowEndpoint.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PJSIPShowEndpoint action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Morvai Szabolcs <morvai88@gmail.com>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://marcelog.github.com/PAMI/
+ *
+ * Copyright 2022 Morvai Szabolcs <morvai88@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+namespace PAMI\Message\Action;
+
+/**
+ * PJSIPShowEndpoint action message.
+ *
+ * PHP Version 5
+ *
+ * @category   Pami
+ * @package    Message
+ * @subpackage Action
+ * @author     Morvai Szabolcs <morvai88@gmail.com>
+ * @license    http://marcelog.github.com/PAMI/ Apache License 2.0
+ * @link       http://marcelog.github.com/PAMI/
+ */
+class PJSIPShowEndpoint extends ActionMessage
+{
+    /**
+     * Constructor.
+     *
+     * @param string $endpoint Endpoint name.
+     *
+     * @return void
+     */
+    public function __construct($endpoint)
+    {
+        parent::__construct('PJSIPShowEndpoint');
+        $this->setKey('Endpoint', $endpoint);
+    }
+}
